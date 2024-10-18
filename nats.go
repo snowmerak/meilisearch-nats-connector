1cfde61d89039417e131fcef8c7eda0842e3282c
snowmerak <snowmerak@outlook.com>
2024-10-18 16:43:41 +0900

    make nats client

---
+++ nats.go
@@ -0,0 +1,52 @@
+package meilisearchnatsconnector
+
+import "github.com/nats-io/nats.go"
+
+type NatsConnectionConfig struct {
+	host string
+	username string
+	password string
+	token string
+}
+
+func NewNatsConnectionConfig() *NatsConnectionConfig {
+	return &NatsConnectionConfig{
+		host: "localhost:4222",
+		username: "",
+		password: "",
+		token: "",
+	}
+}
+
+func (cfg *NatsConnectionConfig) SetHost(host string) *NatsConnectionConfig {
+	cfg.host = host
+	return cfg
+}
+
+func (cfg *NatsConnectionConfig) SetUsername(username string) *NatsConnectionConfig {
+	cfg.username = username
+	return cfg
+}
+
+func (cfg *NatsConnectionConfig) SetPassword(password string) *NatsConnectionConfig {
+	cfg.password = password
+	return cfg
+}
+
+func (cfg *NatsConnectionConfig) SetToken(token string) *NatsConnectionConfig {
+	cfg.token = token
+	return cfg
+}
+
+type NatsConnection struct {
+	conn *nats.Conn
+}
+
+func NewNatsConnection(config *NatsConnectionConfig) (*NatsConnection, error) {
+	conn, err := nats.Connect(config.host, nats.UserInfo(config.username, config.password), nats.Token(config.token))
+	if err != nil {
+		return nil, err
+	}
+
+	return &NatsConnection{conn: conn}, nil
+}
