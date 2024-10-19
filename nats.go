fccee3d6b092df0a68e0defbafa648470ecdd375
snowmerak <snowmerak@outlook.com>
2024-10-20 00:37:31 +0900

    generate uuid in constructor

--- nats.go
+++ nats.go
@@ -4,22 +4,23 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
 )
 
 type NatsConnectionConfig struct {
-	host string
+	host     string
 	username string
 	password string
-	token string
+	token    string
 }
 
 func NewNatsConnectionConfig() *NatsConnectionConfig {
 	return &NatsConnectionConfig{
-		host: "localhost:4222",
+		host:     "localhost:4222",
 		username: "",
 		password: "",
-		token: "",
+		token:    "",
 	}
 }
 
@@ -45,9 +46,15 @@ func (cfg *NatsConnectionConfig) SetToken(token string) *NatsConnectionConfig {
 
 type NatsConnection struct {
 	conn *nats.Conn
+	uuid string
 }
 
 func NewNatsConnection(ctx context.Context, config *NatsConnectionConfig) (*NatsConnection, error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return nil, fmt.Errorf("nats: uuid: %w", err)
+	}
+
 	conn, err := nats.Connect(config.host, nats.UserInfo(config.username, config.password), nats.Token(config.token))
 	if err != nil {
 		return nil, fmt.Errorf("nats: connect: %w", err)
@@ -57,5 +64,8 @@ func NewNatsConnection(ctx context.Context, config *NatsConnectionConfig) (*Nats
 		conn.Close()
 	})
 
-	return &NatsConnection{conn: conn}, nil
+	return &NatsConnection{
+		conn: conn,
+		uuid: id.String(),
+	}, nil
 }
